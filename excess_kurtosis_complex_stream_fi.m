d0330d562cccf0f4751b59fde3d91935faceb78e
Suraj Gowda <sgowda@thundercat.jpl.nasa.gov>
2013-08-26 15:44:47 -0700

    chagned origin moment calcuations to the same format as central moment calcs

--- excess_kurtosis_complex_stream_fi.m
+++ excess_kurtosis_complex_stream_fi.m
@@ -1,5 +1,5 @@
 function [kappa_x, num, den, mean_power] = ...
-    excess_kurtosis_complex_stream_fi2(x_re_fi, x_im_fi)
+    excess_kurtosis_complex_stream_fi(x_re_fi, x_im_fi)
 % excess_kurtosis_complex
 %
 %   kappa_x = excess_kurtosis_complex(x)
@@ -32,19 +32,26 @@ double_type_x_acc = fi_dtype(1, double_type_x.WordLength+acc_len, double_type_x.
 triple_type_x_acc = fi_dtype(1, triple_type_x.WordLength+acc_len, triple_type_x.FractionLength);
 quad_type_x_acc = fi_dtype(1, quad_type_x.WordLength+acc_len, quad_type_x.FractionLength);
 
-%-- Calculate cross products: 5 reg multipliers, 1 large multiplier, and 2 adders
-x_re_sq = x_re_fi.*x_re_fi;
-x_im_sq = x_im_fi.*x_im_fi;
+%-- Calculate origin_moments: 5 reg multipliers, 1 large multiplier, and 2 adders
+[x_re_sq, x_re_sq_dtype] = mult_fi('mult1', x_re_fi, x_re_fi, 'type_a', type_x, 'type_b', type_x);
+[x_im_sq, x_im_sq_dtype] = mult_fi('mult2', x_im_fi, x_im_fi, 'type_a', type_x, 'type_b', type_x);
+% x_re_sq = x_re_fi.*x_re_fi;
+% x_im_sq = x_im_fi.*x_im_fi;
 
-abs_x_sq = x_re_sq + x_im_sq;
-abs_x_4th = abs_x_sq .* abs_x_sq;
+[abs_x_sq, abs_x_sq_dtype] = add_fi('add1', x_re_sq, x_im_sq, 'type_a', x_re_sq_dtype, 'type_b', x_im_sq_dtype);
+[abs_x_4th, abs_x_4th_dtype] = mult_fi('mult3', abs_x_sq, abs_x_sq, 'type_a', abs_x_sq_dtype, 'type_b', abs_x_sq_dtype);
+% abs_x_sq = x_re_sq + x_im_sq;
+% abs_x_4th = abs_x_sq .* abs_x_sq;
 
-x_sq_re = x_re_sq - x_im_sq; % TODO reuse hardware with the abs_x_sq computation!
-x_sq_im_unscaled = x_re_fi.*x_im_fi;
-x_sq_im = scale_fi('', x_sq_im_unscaled, 1);
+[x_sq_re, x_sq_re_dtype] = subtract_fi('add2', x_re_sq, x_im_sq, 'type_a', x_re_sq_dtype, 'type_b', x_im_sq_dtype);
+% x_sq_re = x_re_sq - x_im_sq; % TODO reuse hardware with the abs_x_sq computation!
+% x_sq_im_unscaled = x_re_fi.*x_im_fi;
+[x_sq_im_unscaled, x_sq_im_unscaled_dtype] = mult_fi('mult4', x_re_fi, x_im_fi, 'type_a', type_x, 'type_b', type_x);
+% x_sq_im = scale_fi('', x_sq_im_unscaled, 1);
+[x_sq_im, x_sq_im_dtype] = scale_fi('', x_sq_im_unscaled, 1, 'type_x', x_sq_im_unscaled_dtype);
 
-x_3rd_re = x_re_fi.*abs_x_sq;
-x_3rd_im = x_im_fi.*abs_x_sq;
+[x_3rd_re, x_3rd_re_dtype] = mult_fi('mult4', x_re_fi, abs_x_sq, 'type_a', type_x, 'type_b', abs_x_sq_dtype);
+[x_3rd_im, x_3rd_im_dtype] = mult_fi('mult5', x_im_fi, abs_x_sq, 'type_a', type_x, 'type_b', abs_x_sq_dtype);
 
 %-- Simulate accumulators
 single_acc_dtype = fi_dtype(1, 96, single_type_x.FractionLength);
@@ -86,4 +93,4 @@ x_3rd_acc_re_rounded = quantize(triple_acc_quantizer, x_3rd_acc_re);
 x_3rd_acc_im_rounded = quantize(triple_acc_quantizer, x_3rd_acc_im);
 
 [num, den, mean_power] = kurtosis_moment_calc_fi(m_x_re_rounded, m_x_im_rounded, x_sq_acc_re_rounded, x_sq_acc_im_rounded, abs_x_sq_acc_rounded, abs_x_4th_acc, x_3rd_acc_re_rounded, x_3rd_acc_im_rounded, 'acc_len', acc_len);
-kappa_x = double(num)/double(den);
+kappa_x = double(num)/double(den) - 2;
