391ee315f823df45a450d70d269ecc52719888cc
Suraj Gowda <sgowda@mlspc.jpl.nasa.gov>
2013-08-26 20:15:05 -0700

    accumulate_fi uses built-in sum function, which is properly defined for fixed-point data

--- accumulate_fi.m
+++ accumulate_fi.m
@@ -1,15 +1,5 @@
-function [x_acc] = accumulate_fi(x, dtype)
-% Software implemenation of a fixed-point accumulator 
-% where the bits do NOT grow as more numbers are added,
-% just as in a real hardware implementation
+function [x_acc, x_acc_dtype] = accumulate_fi(x, dtype)
 
-quantizer = fixed.Quantizer('WordLength', dtype.WordLength, ...
-    'FractionLength', dtype.FractionLength, ...
-    'RoundingMethod', 'round', 'OverflowAction', 'saturate');
+x_acc = sum(x);
+x_acc_dtype = fi_dtype(x_acc)
 
-x_acc = fi(0, dtype.Signed, dtype.WordLength, dtype.FractionLength);
-
-for k=1:length(x)
-    x_acc_unr = x_acc + x(k);
-    x_acc = quantize(quantizer, x_acc_unr);
-end
