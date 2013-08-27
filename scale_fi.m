58db29f5ab2757d9fb0d4d344d8f34f1b2422b55
Suraj Gowda <sgowda@mlspc.jpl.nasa.gov>
2013-08-26 20:17:11 -0700

    scale_fi returns data type now?

--- scale_fi.m
+++ scale_fi.m
@@ -1,6 +1,8 @@
-function [x_scale] = scale_fi(name, x_fi, scale_factor)
+function [x_scale, x_scale_dtype] = scale_fi(name, x_fi, scale_factor, varargin)
 
 % Fixed-point scaling by power of 2 (shift binary point)
 new_fraction_length = x_fi.FractionLength - scale_factor;
 x_scale = fi(double(x_fi) * 2^scale_factor, x_fi.Signed, ...
     x_fi.WordLength, new_fraction_length);
+
+x_scale_dtype = extract_fi_dtype(x_scale);
