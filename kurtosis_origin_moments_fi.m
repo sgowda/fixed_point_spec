d37baaa825739d6991b78a54a0fccad1eb1c5f52
Suraj Gowda <sgowda@mlspc.jpl.nasa.gov>
2013-08-26 21:22:05 -0700

    separate function to calculate fixed-point origin moments

---
+++ kurtosis_origin_moments_fi.m
@@ -0,0 +1,20 @@
+function [x_re_fi, x_im_fi, x_sq_re, x_sq_im, abs_x_sq, abs_x_4th, x_3rd_re, x_3rd_im] = kurtosis_origin_moments_fi(x_re_fi, x_im_fi)
+type_x_default = fi_dtype(1, 18, 17);
+defaults = {'type_x', type_x_default};
+type_x = get_var('type_x', 'defaults', defaults, varargin{:});
+
+%-- Calculate origin_moments: 5 reg multipliers, 1 large multiplier, and 2 adders
+[x_re_sq, x_re_sq_dtype] = mult_fi('mult1', x_re_fi, x_re_fi, 'type_a', type_x, 'type_b', type_x);
+[x_im_sq, x_im_sq_dtype] = mult_fi('mult2', x_im_fi, x_im_fi, 'type_a', type_x, 'type_b', type_x);
+
+[abs_x_sq, abs_x_sq_dtype] = add_fi('add1', x_re_sq, x_im_sq, 'type_a', x_re_sq_dtype, 'type_b', x_im_sq_dtype);
+[abs_x_4th, abs_x_4th_dtype] = mult_fi('mult3', abs_x_sq, abs_x_sq, 'type_a', abs_x_sq_dtype, 'type_b', abs_x_sq_dtype);
+
+[x_sq_re, x_sq_re_dtype] = subtract_fi('add2', x_re_sq, x_im_sq, 'type_a', x_re_sq_dtype, 'type_b', x_im_sq_dtype);
+[x_sq_im_unscaled, x_sq_im_unscaled_dtype] = mult_fi('mult4', x_re_fi, x_im_fi, 'type_a', type_x, 'type_b', type_x);
+[x_sq_im, x_sq_im_dtype] = scale_fi('', x_sq_im_unscaled, 1, 'type_x', x_sq_im_unscaled_dtype);
+
+[x_3rd_re, x_3rd_re_dtype] = mult_fi('mult4', x_re_fi, abs_x_sq, 'type_a', type_x, 'type_b', abs_x_sq_dtype);
+[x_3rd_im, x_3rd_im_dtype] = mult_fi('mult5', x_im_fi, abs_x_sq, 'type_a', type_x, 'type_b', abs_x_sq_dtype);
+
+end
